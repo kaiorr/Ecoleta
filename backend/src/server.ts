2312a4490d4ddab0fa70582bdff30d4b5f5ac368
kaiorr <kaio.rocharibeiro@gmail.com>
2020-06-02 19:39:42 -0300

    feat: arquvo de configuração do express

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,14 +1,9 @@
-import express from "express"
+import express from 'express'
+import routes from './routes'
 
 const app = express()
 
-app.get('/users', (req, res) => {
-  console.log('listagem de Users')
+app.use(express.json())
+app.use(routes)
 
-  res.json([
-    'Kaio',
-    'Linux'
-  ])
-})
-
-app.listen(3333)  
+app.listen(3333)
