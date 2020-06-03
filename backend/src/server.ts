826195a62ed4d2a36317eae4c61bdd4ee41a69b1
kaiorr <kaio.rocharibeiro@gmail.com>
2020-06-02 21:46:34 -0300

    feat: criação do arquivo de seeds, e também import na rota das imagens

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,12 @@
 import express from 'express'
 import routes from './routes'
+import path from 'path'
 
 const app = express()
 
 app.use(express.json())
 app.use(routes)
+
+app.use('/assets', express.static(path.resolve(__dirname, '..', 'assets')))
 
 app.listen(3333)
