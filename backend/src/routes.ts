26f9c0d76ae7babbc33c3648e6c41c606dcf5175
kaiorr <kaio.rocharibeiro@gmail.com>
2020-06-03 20:39:43 -0300

    feat: definido as rotas

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,55 +1,16 @@
 import express from "express"
-import knex from './database/connection'
+
+import PointsController from "./controllers/PointsController"
+import ItemsController from "./controllers/ItemsController"
+
 
 const routes = express.Router()
+const pointsController = new PointsController()
+const itemsController = new ItemsController()
 
-routes.get('/itens', async (req, res) => {
-  const itens = await knex('itens').select('*')
-
-  const serializedItens = itens.map(item => {
-    return {
-      id: item.id,
-      name: item.name,
-      image_url: `http://localhost:3333/uploads/${item.image}`,
-    }
-  })
-
-  return res.json(serializedItens)
-})
-
-routes.post('/points', async (req, res) => {
-  const {
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf,
-    itens
-  } = req.body
-
-  await knex('points').insert({
-    image: 'image-fake',
-    name,
-    email,
-    whatsapp,
-    latitude,
-    longitude,
-    city,
-    uf
-  })
-
-  const pointItens = itens.map((item_id: number) => {
-    return {
-      item_id,
-      point_id: ids[0],
-    }
-  })
-
-  await knex('point_itens').insert(pointItens)
-
-  return res.json({ success: true })
-})
+routes.get('/items', itemsController.index)
+routes.post('/points', pointsController.create)
+routes.get('/points/:id', pointsController.show)
+routes.get('/points', pointsController.index)
 
 export default routes
