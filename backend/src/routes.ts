268e98876a7270c051ad809d814fe3721fb40fe6
kaiorr <kaio.rocharibeiro@gmail.com>
2020-06-02 22:40:58 -0300

    feat: finalizado segundo dia do Next Level Week

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,55 @@
 import express from "express"
+import knex from './database/connection'
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  return res.json({ messsage: 'Hello World' })
+routes.get('/itens', async (req, res) => {
+  const itens = await knex('itens').select('*')
+
+  const serializedItens = itens.map(item => {
+    return {
+      id: item.id,
+      name: item.name,
+      image_url: `http://localhost:3333/uploads/${item.image}`,
+    }
+  })
+
+  return res.json(serializedItens)
+})
+
+routes.post('/points', async (req, res) => {
+  const {
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf,
+    itens
+  } = req.body
+
+  await knex('points').insert({
+    image: 'image-fake',
+    name,
+    email,
+    whatsapp,
+    latitude,
+    longitude,
+    city,
+    uf
+  })
+
+  const pointItens = itens.map((item_id: number) => {
+    return {
+      item_id,
+      point_id: ids[0],
+    }
+  })
+
+  await knex('point_itens').insert(pointItens)
+
+  return res.json({ success: true })
 })
 
 export default routes
