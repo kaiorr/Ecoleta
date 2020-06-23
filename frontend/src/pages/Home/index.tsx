1f132e85a3c78131e835f252469159e9a02c2cdd
kaiorr <kaio.rocharibeiro@gmail.com>
2020-06-23 20:11:36 -0300

    feat: finalizado todo html

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {FiLogIn} from 'react-icons/fi'
+import {Link} from 'react-router-dom'
 import logo from '../../assets/logo.svg'
 import './styles.css'
 
@@ -6,7 +8,21 @@ const Home = () => {
   return (
     <div id="page-home">
       <div className="content">
-        <img src={logo} alt="Ecoleta"/>
+        <header>
+          <img src={logo} alt="Ecoleta"/>
+        </header>
+        
+        <main>
+          <h1>Seu marketplace e coleta de resíduos</h1>
+          <p>Ajudamos pessoas a encontrar pontos de coleta de forma eficiente.</p>
+
+          <Link to="/create-point">
+            <span>
+              <FiLogIn />
+            </span>
+            <strong>Cadastre um ponto de coleta</strong>
+            </Link>
+        </main>
       </div>
     </div>
   )
